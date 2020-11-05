e080a296fde6480bc481475eb20222986641bd14
moisesu05 <moisesronanl@gmail.com>
2020-11-05 22:02:00 +0800

    Updates

---
+++ precache-manifest.883878b6121bd87ae2583e3db2a9c580.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa07b51fe9db43fa369e124aa0264c83",
+    "url": "/react-sample-company-web/index.html"
+  },
+  {
+    "revision": "93be32f9ca902591ae44",
+    "url": "/react-sample-company-web/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6a5c7f1dd3f4978aa17f",
+    "url": "/react-sample-company-web/static/js/2.dcf3b52e.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/react-sample-company-web/static/js/2.dcf3b52e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93be32f9ca902591ae44",
+    "url": "/react-sample-company-web/static/js/main.9460ea69.chunk.js"
+  },
+  {
+    "revision": "21897c7b36376ef358e3",
+    "url": "/react-sample-company-web/static/js/runtime-main.11383507.js"
+  },
+  {
+    "revision": "eda4598198a00832fb334d5a4e588ead",
+    "url": "/react-sample-company-web/static/media/01-thumbnail.eda45981.jpg"
+  },
+  {
+    "revision": "e52270b3af9b353d02078b01acd931ae",
+    "url": "/react-sample-company-web/static/media/02-thumbnail.e52270b3.jpg"
+  },
+  {
+    "revision": "09c41872ba6c96c6efdcab7f202a71a7",
+    "url": "/react-sample-company-web/static/media/03-thumbnail.09c41872.jpg"
+  },
+  {
+    "revision": "80c8ff5c4a1a0775616a5541086b1d51",
+    "url": "/react-sample-company-web/static/media/04-thumbnail.80c8ff5c.jpg"
+  },
+  {
+    "revision": "68d8d9495f59fa5109f3b337d784cacc",
+    "url": "/react-sample-company-web/static/media/05-thumbnail.68d8d949.jpg"
+  },
+  {
+    "revision": "20456fff3658b40a59a73ae9821a273f",
+    "url": "/react-sample-company-web/static/media/06-thumbnail.20456fff.jpg"
+  },
+  {
+    "revision": "e0b34f6fc62c82231d2016e7e85d7ed0",
+    "url": "/react-sample-company-web/static/media/1.e0b34f6f.jpg"
+  },
+  {
+    "revision": "cc41e47b8fbdb1c81c375b65071e478c",
+    "url": "/react-sample-company-web/static/media/2.cc41e47b.jpg"
+  },
+  {
+    "revision": "2df43e3546ec0d6666badb2ddbc716ee",
+    "url": "/react-sample-company-web/static/media/3.2df43e35.jpg"
+  },
+  {
+    "revision": "2047255605dd443beb3c757c3cbe702e",
+    "url": "/react-sample-company-web/static/media/about-bg.20472556.jpg"
+  },
+  {
+    "revision": "e2d2a51c2932205b52f4a5ae1943eb97",
+    "url": "/react-sample-company-web/static/media/header-bg.e2d2a51c.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/react-sample-company-web/precache-manifest.883878b6121bd87ae2583e3db2a9c580.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-sample-company-web/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
